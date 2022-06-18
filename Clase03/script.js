7424c651bbc4c9524e244b0c1d0f66d4f75c2db6
ivan <ivanmassenzio@gmail.com>
2022-06-18 17:43:54 -0300

    1er ejercicio

---
+++ Clase03/script.js
@@ -0,0 +1,21 @@
+let opcion = 0
+let time = new Date();
+
+do {
+    console.log("1. Preguntar la hora")
+    console.log("2. Preguntar el mes")
+    console.log("3. Preguntar el año")
+    console.log("9. Salir")
+
+    opcion = parseInt(prompt("Ingrese una opcion: "))
+
+    if (opcion == 1){
+        alert("Son las: "+ time.getHours())
+    }
+    if (opcion == 2){
+        alert("El dia es: "+ time.getDay())
+    }
+    if (opcion == 3){
+        alert("El año es: "+ time.getFullYear())
+    }
+} while (opcion !== 9)
