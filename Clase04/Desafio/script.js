f233f2c2dfb6070795388ddd00ff139bd7df5047
ivan <ivanmassenzio@gmail.com>
2022-06-29 09:29:35 -0300

    se crea clase Articulo y Array Articulos

--- Clase04/Desafio/script.js
+++ Clase04/Desafio/script.js
@@ -1,20 +1,49 @@
-// Constructor de articulos
-function Articulo(nombre, precio){
-    this.nombre = nombre
-    this.precio = precio
-    // Arrow Function para calular el monto a pagar
-    this.calcularMonto = (precio, cuotas) => precio / cuotas
+// Clase Articulos
+
+class Articulo {
+    constructor(id, nombre, precio) {
+        this.id = id
+        this.nombre = nombre
+        this.precio = precio
+    }
+    calcularMonto = (precio, cuotas) => precio / cuotas
 }
 
+
+
+
+// // Constructor de articulos
+// function Articulo(nombre, precio){
+//     this.nombre = nombre
+//     this.precio = precio
+//     // Arrow Function para calular el monto a pagar
+//     this.calcularMonto = (precio, cuotas) => precio / cuotas
+// }
+
 // Creacion de 3 articulos
-const mate = new Articulo("Mate", 300)
-const termo = new Articulo("Termo", 500)
-const mochila = new Articulo("Mochila", 1500)
+const mate = new Articulo(1, "Mate", 300)
+const termo = new Articulo(2, "Termo", 500)
+const mochila = new Articulo(3, "Mochila", 1500)
 
+//  Creacion array de objetos
+const articulos = new Array(mate,termo)
 
+// Agrego un articulo mas al array
+articulos.push(mochila)
+// let menu = ""
+// articulos.forEach(element => {
+//     menu += `${element.id} ${element.nombre}\n`
+// });
+
+// prompt(`Bienvenido, seleccione un articulo (9 para salir)\n ${menu}`)
 // Menu principal
 let mostarMenu = () => {
-    let opcion = parseInt(prompt("Bienvenido, seleccione un articulo (9 para salir)\n 1 Mate\n 2 Termo\n 3 Mochila"))
+    let menu = ""
+    articulos.forEach(element => {
+    menu += `${element.id} ${element.nombre}\n`
+    });
+
+    let opcion = parseInt(prompt(`Bienvenido, seleccione un articulo (9 para salir)\n ${menu}`))
     return opcion
 }
 
@@ -48,4 +77,3 @@ while (opcionSeleccionada !== 9) {
     }
     opcionSeleccionada = mostarMenu()
 }
-
