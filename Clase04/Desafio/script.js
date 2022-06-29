97caa36b319c3e3025558d88445964b5f1b0ecbf
ivan <ivanmassenzio@gmail.com>
2022-06-29 12:35:36 -0300

    se modifico la logica del menuPago

--- Clase04/Desafio/script.js
+++ Clase04/Desafio/script.js
@@ -1,5 +1,4 @@
 // Clase Articulos
-
 class Articulo {
     constructor(id, nombre, precio) {
         this.id = id
@@ -8,17 +7,6 @@ class Articulo {
     }
     calcularMonto = (precio, cuotas) => precio / cuotas
 }
-
-
-
-
-// // Constructor de articulos
-// function Articulo(nombre, precio){
-//     this.nombre = nombre
-//     this.precio = precio
-//     // Arrow Function para calular el monto a pagar
-//     this.calcularMonto = (precio, cuotas) => precio / cuotas
-// }
 
 // Creacion de 3 articulos
 const mate = new Articulo(1, "Mate", 300)
@@ -30,12 +18,7 @@ const articulos = new Array(mate,termo)
 
 // Agrego un articulo mas al array
 articulos.push(mochila)
-// let menu = ""
-// articulos.forEach(element => {
-//     menu += `${element.id} ${element.nombre}\n`
-// });
 
-// prompt(`Bienvenido, seleccione un articulo (9 para salir)\n ${menu}`)
 // Menu principal
 let mostarMenu = () => {
     let menu = ""
@@ -49,17 +32,14 @@ let mostarMenu = () => {
 
 let menuPago = (articulo) => {
     let opcionPago = parseInt(prompt(`El articulo ${articulo.nombre} cuesta ${articulo.precio}, en cuantas cuotas quiere pagarlo? (1-3)`))
-    switch (opcionPago){
-        case 1:
-        case 2:
-        case 3:
-            let cuotas = articulo.calcularMonto(articulo.precio, opcionPago)
-            alert("Serian " + opcionPago + " cuotas de " + cuotas)
-            break
-        default:
-            alert("cantidad de cuotas invalidas")
-            break
-    }    
+    console.log(opcionPago)
+    if(!(opcionPago === 1 || opcionPago === 2 || opcionPago === 3)){
+        alert("Cantidad de cuotas invalidas")
+    }
+    else{
+        let cuotas = articulo.calcularMonto(articulo.precio, opcionPago)
+        alert("Serian " + opcionPago + " cuotas de " + cuotas)
+    }
 }
 
 let opcionSeleccionada = mostarMenu()
@@ -67,13 +47,13 @@ let opcionSeleccionada = mostarMenu()
 while (opcionSeleccionada !== 9) {
     let opcionPago = 0
     if (opcionSeleccionada == 1) {
-        menuPago(mate)       
+        menuPago(articulos[0])       
     }
     if (opcionSeleccionada == 2) {
-        menuPago(termo)
+        menuPago(articulos[1])
     }
     if (opcionSeleccionada == 3) {
-        menuPago(mochila)
+        menuPago(articulos[2])
     }
     opcionSeleccionada = mostarMenu()
 }
